eb59f009adf40022216ce1882c69c6fd034eae6a
Joseph Tong <josephtong710@gmail.com>
2026-03-19 02:03:06 -0700

    done

--- LinkedQueue.tpp
+++ LinkedQueue.tpp
@@ -1,6 +1,5 @@
 template <typename T>
 LinkedQueue<T>::LinkedQueue() {
-    // TODO
     this->head = nullptr;
     this->last = nullptr;
     this->length = 0;
@@ -8,6 +7,9 @@ LinkedQueue<T>::LinkedQueue() {
 
 template <typename T>
 LinkedQueue<T>::LinkedQueue(const LinkedQueue<T>& copyObj) {
+    this->head = nullptr;
+    this->last = nullptr;
+    this->length = 0;
     copy(copyObj);
 }
 
@@ -26,65 +28,57 @@ LinkedQueue<T>::~LinkedQueue() {
 }
 
 template <typename T>
+T LinkedQueue<T>::front() const {
+    if (isEmpty())
+        throw string("error, queue is empty, cannot access the front");
+    return this->head->value;
+}
+
+template <typename T>
 T LinkedQueue<T>::back() const {
-    // TODO
-    if (isEmpty()) {
+    if (isEmpty())
         throw string("error, queue is empty, cannot access the back");
-    }
     return this->last->value;
 }
 
 template <typename T>
-void LinkedQueue<T>::clear() {
-    // TODO
-    while (!isEmpty()) {
-        dequeue();
+void LinkedQueue<T>::enqueue(const T& elem) {
+    Node* newNode = new Node(elem);
+    if (isEmpty()) {
+        this->head = newNode;
+        this->last = newNode;
+    } else {
+        this->last->next = newNode;
+        this->last = newNode;
     }
-}
-
-template <typename T>
-void LinkedQueue<T>::copy(const LinkedQueue<T>& copyObj) {
-    // TODO
-        Node* current = copyObj.head;
-    while (current != nullptr) {
-        enqueue(current->value);
-        current = current->next;
-    }
+    this->length++;
 }
 
 template <typename T>
 void LinkedQueue<T>::dequeue() {
-    // TODO
-        if (isEmpty()) {
-            throw string("error, queue is empty, cannot dequeue");
-        }
-    Node* temp = head;
-    head = head->next;
+    if (isEmpty())
+        throw string("error, queue is empty, cannot dequeue");
+    Node* temp = this->head;
+    this->head = this->head->next;
+    if (this->head == nullptr)
+        this->last = nullptr;
     delete temp;
     this->length--;
 }
 
 template <typename T>
-void LinkedQueue<T>::enqueue(const T& elem) {
-    // TODO
-    Node* newNode = new Node(elem);
-    if (isEmpty()) {
-        head = this->last = newNode;
-    } else {
-        this->last->next = newNode;
-        this->last= newNode;
-    }
-   this->length++;
-
+void LinkedQueue<T>::clear() {
+    while (!isEmpty())
+        dequeue();
 }
 
 template <typename T>
-T LinkedQueue<T>::front() const {
-    // TODO
-    if (isEmpty()) {
-       throw string("error, queue is empty, cannot access the front");
+void LinkedQueue<T>::copy(const LinkedQueue<T>& copyObj) {
+    Node* curr = copyObj.head;
+    while (curr != nullptr) {
+        enqueue(curr->value);
+        curr = curr->next;
     }
-    return head->value;
 }
 
 template <typename T>
--- main.cpp
+++ main.cpp
@@ -1,18 +1,61 @@
-#include <iostream>
 #include "LinkedQueue.hpp"
+#include <iostream>
 using namespace std;
-int main(){
-    LinkedQueue<int> queue;
-    queue.enqueue(10);
-    queue.enqueue(20);
-    queue.enqueue(30);
-    cout << "Front element: " << queue.front() << endl; // Should print 10
-    cout << "Back element: " << queue.back() << endl;   // Should print 30
-    cout << "Queue length: " << queue.getLength() << endl; // Should print 3
-    queue.dequeue();
-    cout << "Front element after dequeue: " << queue.front() << endl; // Should print 20
-    cout << "Queue length after dequeue: " << queue.getLength() << endl; // Should print 2
-    queue.clear();
-    cout << "Is queue empty after clear? " << (queue.isEmpty() ? "Yes" : "No") << endl; // Should print Yes
+
+int main() {
+    LinkedQueue<int> queueA;
+    const int MAX_SIZE = 10;
+
+    // Test front/back on empty queue
+    try { cout << queueA.front() << " "; } catch (string& e) { cout << e << " "; }
+    try { cout << queueA.back()  << " "; } catch (string& e) { cout << e << " "; }
+
+    // Single element
+    queueA.enqueue(-5);
+    cout << "A length: " << queueA.getLength() << " ";
+    cout << "A front: "  << queueA.front()     << " ";
+    cout << "A back: "   << queueA.back()      << " ";
+
+    queueA.dequeue();
+    cout << "A length: " << queueA.getLength() << " ";
+    try { cout << queueA.front() << " "; } catch (string& e) { cout << e << " "; }
+    try { cout << queueA.back()  << " "; } catch (string& e) { cout << e << " "; }
+
+    // Enqueue 0-9
+    for (int i = 0; i < MAX_SIZE; i++) queueA.enqueue(i);
+
+    cout << "A length: " << queueA.getLength() << " ";
+    cout << "A front: "  << queueA.front()     << " ";
+    cout << "A back: "   << queueA.back()      << " ";
+
+    // Copy A into B (copy constructor)
+    LinkedQueue<int> queueB = queueA;
+
+    // Drain A
+    cout << "A contains: ";
+    for (int i = 0; i < MAX_SIZE; i++) { cout << queueA.front() << ' '; queueA.dequeue(); }
+    cout << " ";
+
+    if (queueA.isEmpty()) cout << "A is now empty ";
+    else                  cout << "A is not empty [ERROR] ";
+
+    // Inspect B
+    cout << "B length: " << queueB.getLength() << " ";
+    cout << "B front: "  << queueB.front()     << " ";
+    cout << "B back: "   << queueB.back()      << " ";
+
+    // Copy B into C (assignment operator), then clear B
+    LinkedQueue<int> queueC;
+    queueC = queueB;
+    queueB.clear();
+
+    if (queueB.isEmpty()) cout << "B is now empty ";
+    else                  cout << "B is not empty [ERROR] ";
+
+    // Inspect C
+    cout << "C length: " << queueC.getLength() << " ";
+    cout << "C front: "  << queueC.front()     << " ";
+    cout << "C back: "   << queueC.back()      << " ";
+
     return 0;
 }
